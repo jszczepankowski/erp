95f2b2531bebaeb5e040b5a3ce711d67037acfb0
jszczepankowski <93668299+jszczepankowski@users.noreply.github.com>
2026-03-20 18:46:57 +0100

    Add Sprint 6 reporting and calendar features

---
+++ erp-omd/includes/class-plugin.php
@@ -0,0 +1,136 @@
+<?php
+
+class ERP_OMD_Plugin
+{
+    private $role_repository;
+    private $employee_repository;
+    private $salary_repository;
+    private $client_repository;
+    private $client_rate_repository;
+    private $project_repository;
+    private $estimate_repository;
+    private $estimate_item_repository;
+    private $project_note_repository;
+    private $project_rate_repository;
+    private $project_cost_repository;
+    private $project_financial_repository;
+    private $time_entry_repository;
+    private $monthly_hours_service;
+    private $employee_service;
+    private $client_project_service;
+    private $estimate_service;
+    private $time_entry_service;
+    private $project_financial_service;
+    private $reporting_service;
+    private $admin;
+    private $rest_api;
+
+    public function __construct()
+    {
+        $this->role_repository = new ERP_OMD_Role_Repository();
+        $this->employee_repository = new ERP_OMD_Employee_Repository();
+        $this->salary_repository = new ERP_OMD_Salary_History_Repository();
+        $this->client_repository = new ERP_OMD_Client_Repository();
+        $this->client_rate_repository = new ERP_OMD_Client_Rate_Repository();
+        $this->project_repository = new ERP_OMD_Project_Repository();
+        $this->estimate_repository = new ERP_OMD_Estimate_Repository();
+        $this->estimate_item_repository = new ERP_OMD_Estimate_Item_Repository();
+        $this->project_note_repository = new ERP_OMD_Project_Note_Repository();
+        $this->project_rate_repository = new ERP_OMD_Project_Rate_Repository();
+        $this->project_cost_repository = new ERP_OMD_Project_Cost_Repository();
+        $this->project_financial_repository = new ERP_OMD_Project_Financial_Repository();
+        $this->time_entry_repository = new ERP_OMD_Time_Entry_Repository();
+        $this->monthly_hours_service = new ERP_OMD_Monthly_Hours_Service();
+        $this->employee_service = new ERP_OMD_Employee_Service(
+            $this->employee_repository,
+            $this->salary_repository,
+            $this->monthly_hours_service
+        );
+        $this->client_project_service = new ERP_OMD_Client_Project_Service(
+            $this->client_repository,
+            $this->employee_repository,
+            $this->role_repository
+        );
+        $this->estimate_service = new ERP_OMD_Estimate_Service(
+            $this->estimate_repository,
+            $this->estimate_item_repository,
+            $this->client_repository,
+            $this->project_repository
+        );
+        $this->time_entry_service = new ERP_OMD_Time_Entry_Service(
+            $this->time_entry_repository,
+            $this->employee_repository,
+            $this->project_repository,
+            $this->role_repository,
+            $this->client_rate_repository,
+            $this->project_rate_repository,
+            $this->salary_repository
+        );
+        $this->project_financial_service = new ERP_OMD_Project_Financial_Service(
+            $this->project_repository,
+            $this->project_cost_repository,
+            $this->project_financial_repository,
+            $this->time_entry_repository
+        );
+        $this->reporting_service = new ERP_OMD_Reporting_Service(
+            $this->project_repository,
+            $this->client_repository,
+            $this->employee_repository,
+            $this->project_cost_repository,
+            $this->time_entry_repository,
+            $this->project_financial_service
+        );
+        $this->admin = new ERP_OMD_Admin(
+            $this->role_repository,
+            $this->employee_repository,
+            $this->salary_repository,
+            $this->employee_service,
+            $this->monthly_hours_service,
+            $this->client_repository,
+            $this->client_rate_repository,
+            $this->project_repository,
+            $this->estimate_repository,
+            $this->estimate_item_repository,
+            $this->project_note_repository,
+            $this->client_project_service,
+            $this->estimate_service,
+            $this->project_rate_repository,
+            $this->project_cost_repository,
+            $this->project_financial_repository,
+            $this->time_entry_repository,
+            $this->time_entry_service,
+            $this->project_financial_service,
+            $this->reporting_service
+        );
+        $this->rest_api = new ERP_OMD_REST_API(
+            $this->role_repository,
+            $this->employee_repository,
+            $this->salary_repository,
+            $this->employee_service,
+            $this->monthly_hours_service,
+            $this->client_repository,
+            $this->client_rate_repository,
+            $this->project_repository,
+            $this->estimate_repository,
+            $this->estimate_item_repository,
+            $this->project_note_repository,
+            $this->client_project_service,
+            $this->estimate_service,
+            $this->project_rate_repository,
+            $this->project_cost_repository,
+            $this->project_financial_repository,
+            $this->time_entry_repository,
+            $this->time_entry_service,
+            $this->project_financial_service,
+            $this->reporting_service
+        );
+    }
+
+    public function boot()
+    {
+        ERP_OMD_Installer::maybe_upgrade();
+        ERP_OMD_Capabilities::register_hooks();
+        $this->admin->register_hooks();
+        $this->rest_api->register_hooks();
+    }
+}
