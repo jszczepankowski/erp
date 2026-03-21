fb3048b0e691bc9965f353c0110d873bde74f610
jszczepankowski <93668299+jszczepankowski@users.noreply.github.com>
2026-03-21 16:34:46 +0100

    Make worker form and entries full width

--- erp-omd/includes/class-autoloader.php
+++ erp-omd/includes/class-autoloader.php
@@ -10,6 +10,7 @@ class ERP_OMD_Autoloader
         'ERP_OMD_Installer' => 'includes/class-installer.php',
         'ERP_OMD_Capabilities' => 'includes/class-capabilities.php',
         'ERP_OMD_Admin' => 'includes/class-admin.php',
+        'ERP_OMD_Frontend' => 'includes/class-frontend.php',
         'ERP_OMD_REST_API' => 'includes/class-rest-api.php',
         'ERP_OMD_Role_Repository' => 'includes/repositories/class-role-repository.php',
         'ERP_OMD_Employee_Repository' => 'includes/repositories/class-employee-repository.php',
