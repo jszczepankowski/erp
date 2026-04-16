dc755e80153b4f76d3ee898a61b5c334b7cb94db
jszczepankowski <93668299+jszczepankowski@users.noreply.github.com>
2026-04-16 03:39:54 +0200

    ksef: show imported cost invoices in cost tab

--- erp-omd/includes/class-cron-manager.php
+++ erp-omd/includes/class-cron-manager.php
@@ -6,6 +6,8 @@ class ERP_OMD_Cron_Manager
     const MISSING_HOURS_HOOK = 'erp_omd_daily_missing_hours_notifications';
     const PROJECT_DEADLINE_HOOK = 'erp_omd_daily_project_deadline_notifications';
     const GOOGLE_CALENDAR_SYNC_HOOK = 'erp_omd_google_calendar_sync';
+    const KSEF_RETRY_PIPELINE_HOOK = 'erp_omd_ksef_retry_pipeline';
+    const KSEF_API_SYNC_HOOK = 'erp_omd_ksef_api_sync';
 
     public static function register_hooks()
     {
@@ -14,6 +16,8 @@ class ERP_OMD_Cron_Manager
         add_action(self::MISSING_HOURS_HOOK, [__CLASS__, 'run_missing_hours_notifications']);
         add_action(self::PROJECT_DEADLINE_HOOK, [__CLASS__, 'run_project_deadline_notifications']);
         add_action(self::GOOGLE_CALENDAR_SYNC_HOOK, [__CLASS__, 'run_google_calendar_sync']);
+        add_action(self::KSEF_RETRY_PIPELINE_HOOK, [__CLASS__, 'run_ksef_retry_pipeline']);
+        add_action(self::KSEF_API_SYNC_HOOK, [__CLASS__, 'run_ksef_api_sync']);
         self::schedule_events();
     }
 
@@ -28,6 +32,8 @@ class ERP_OMD_Cron_Manager
         wp_clear_scheduled_hook(self::MISSING_HOURS_HOOK);
         wp_clear_scheduled_hook(self::PROJECT_DEADLINE_HOOK);
         wp_clear_scheduled_hook(self::GOOGLE_CALENDAR_SYNC_HOOK);
+        wp_clear_scheduled_hook(self::KSEF_RETRY_PIPELINE_HOOK);
+        wp_clear_scheduled_hook(self::KSEF_API_SYNC_HOOK);
     }
 
     public static function register_weekly_schedule($schedules)
@@ -43,6 +49,12 @@ class ERP_OMD_Cron_Manager
             $schedules['erp_omd_two_hours'] = [
                 'interval' => 2 * HOUR_IN_SECONDS,
                 'display' => 'ERP OMD Every 2 Hours',
+            ];
+        }
+        if (! isset($schedules['erp_omd_five_minutes'])) {
+            $schedules['erp_omd_five_minutes'] = [
+                'interval' => 5 * MINUTE_IN_SECONDS,
+                'display' => 'ERP OMD Every 5 Minutes',
             ];
         }
 
@@ -63,6 +75,12 @@ class ERP_OMD_Cron_Manager
         }
         if (! wp_next_scheduled(self::GOOGLE_CALENDAR_SYNC_HOOK)) {
             wp_schedule_event(time() + HOUR_IN_SECONDS, 'erp_omd_two_hours', self::GOOGLE_CALENDAR_SYNC_HOOK);
+        }
+        if (! wp_next_scheduled(self::KSEF_RETRY_PIPELINE_HOOK)) {
+            wp_schedule_event(time() + 5 * MINUTE_IN_SECONDS, 'erp_omd_five_minutes', self::KSEF_RETRY_PIPELINE_HOOK);
+        }
+        if (! wp_next_scheduled(self::KSEF_API_SYNC_HOOK)) {
+            wp_schedule_event(time() + HOUR_IN_SECONDS, 'hourly', self::KSEF_API_SYNC_HOOK);
         }
     }
 
@@ -197,6 +215,60 @@ class ERP_OMD_Cron_Manager
         $service->sync_all_projects();
     }
 
+
+    public static function run_ksef_retry_pipeline()
+    {
+        $invoice_repository = new ERP_OMD_Cost_Invoice_Repository();
+        $audit_repository = new ERP_OMD_Cost_Invoice_Audit_Repository();
+        $supplier_repository = new ERP_OMD_Supplier_Repository();
+        $workflow = new ERP_OMD_Cost_Invoice_Workflow_Service(
+            $invoice_repository,
+            $audit_repository,
+            $supplier_repository,
+            new ERP_OMD_Project_Repository()
+        );
+        $service = new ERP_OMD_KSeF_Import_Service($workflow, $invoice_repository, $audit_repository, null, null, $supplier_repository);
+        $service->process_retry_queue(20);
+    }
+
+    public static function run_ksef_api_sync()
+    {
+        $invoice_repository = new ERP_OMD_Cost_Invoice_Repository();
+        $audit_repository = new ERP_OMD_Cost_Invoice_Audit_Repository();
+        $supplier_repository = new ERP_OMD_Supplier_Repository();
+        $workflow = new ERP_OMD_Cost_Invoice_Workflow_Service(
+            $invoice_repository,
+            $audit_repository,
+            $supplier_repository,
+            new ERP_OMD_Project_Repository()
+        );
+        $import_service = new ERP_OMD_KSeF_Import_Service(
+            $workflow,
+            $invoice_repository,
+            $audit_repository,
+            null,
+            null,
+            $supplier_repository,
+            new ERP_OMD_Client_Repository()
+        );
+        $sync_service = new ERP_OMD_KSeF_API_Sync_Service($import_service, (string) get_option('erp_omd_company_nip', ''));
+        $result = $sync_service->run_scheduled_sync();
+
+        $last_sync_at = (string) get_option(ERP_OMD_KSeF_API_Sync_Service::OPTION_LAST_SYNC_AT, '');
+        $alert_hours = max(1, (int) get_option(ERP_OMD_KSeF_API_Sync_Service::OPTION_ALERT_AFTER_HOURS, 24));
+        if ($last_sync_at !== '' && strtotime($last_sync_at) < (time() - ($alert_hours * HOUR_IN_SECONDS))) {
+            wp_mail(
+                sanitize_email((string) get_option('admin_email', '')),
+                __('[ERP OMD] KSeF sync alert >24h', 'erp-omd'),
+                sprintf(
+                    __('Brak udanej synchronizacji KSeF od %1$s. Ostatni błąd: %2$s', 'erp-omd'),
+                    $last_sync_at,
+                    (string) ($result['last_error'] ?? get_option(ERP_OMD_KSeF_API_Sync_Service::OPTION_LAST_ERROR, ''))
+                )
+            );
+        }
+    }
+
     private static function notification_settings()
     {
         $defaults = [
@@ -250,6 +322,25 @@ class ERP_OMD_Cron_Manager
 
         $threshold = $today->modify('-' . (int) $settings['after_days'] . ' days');
         return $last_date->format('Y-m-d') <= $threshold->format('Y-m-d');
+    }
+
+
+    public static function restore_backup_bundle_from_zip($zip_path)
+    {
+        if (class_exists('ERP_OMD_Backup_Manager') && method_exists('ERP_OMD_Backup_Manager', 'restore_backup_bundle_from_zip')) {
+            return ERP_OMD_Backup_Manager::restore_backup_bundle_from_zip($zip_path);
+        }
+
+        return ['success' => false, 'error' => 'backup_restore_not_available'];
+    }
+
+    public static function import_sql_dump($sql_path)
+    {
+        if (class_exists('ERP_OMD_Backup_Manager') && method_exists('ERP_OMD_Backup_Manager', 'import_sql_dump')) {
+            return ERP_OMD_Backup_Manager::import_sql_dump($sql_path);
+        }
+
+        return false;
     }
 
     private static function render_template($template, array $employee, $last_entry_date)
